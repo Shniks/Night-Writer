6e78ba17b52a072537bd39d95f4ea1694e85f8e6
Lihkin <shniks@gmail.com>
2017-12-12 17:52:45 -0700

    Completed code for English to Braille.

--- lib/night_write.rb
+++ lib/night_write.rb
@@ -3,17 +3,25 @@ require "./lib/dictionary"
 
 class NightWrite
 
-  attr_reader :dictionary, :count
+  attr_reader   :dictionary
+  attr_accessor :braille, :braille_wrap, :result, :first_row, :second_row, :third_row
+
 
   def initialize
 
     @count = 0
+    @i = 0
+    @braille = []
+    @result =[]
+    @braille_wrap = Hash.new
+    @first_row =[]
+    @second_row = []
+    @third_row = []
     @dictionary = Dictionary.new.dictionary
 
   end
 
   def english_returns_braille_array(incoming_text)
-    braille = []
     incoming_text_split_to_letters = incoming_text.split('')
     incoming_text_split_to_letters.each do |letter|
       if letter.upcase == letter && letter != " "
@@ -28,28 +36,25 @@ class NightWrite
 
   def wrap_every_eighty_strings(braille)
     wrap_length = braille.length
-    braille_wrap = Hash.new
     until wrap_length <= 80 do
-      count += 1
-      braille_wrap[count] = braille.slice!(0..79)
+      @count += 1
+      braille_wrap[@count] = braille.slice!(0..79)
       wrap_length -= 80
     end
-    count += 1
-    braille_wrap[count] = braille
+    @count += 1
+    braille_wrap[@count] = braille
   end
 
   def converts_braille_hash_to_braille(braille_wrap)
-    result = []
-    i = 0
-    until i == braille_wrap.length do
-      first_row =[]
+    until @i == braille_wrap.length do
+      first_row = []
       second_row = []
       third_row = []
-      i = i + 1
-      braille_wrap[i].each_with_index do |data, index|
-        first_row << braille_wrap[i][index][0..1]
-        second_row << braille_wrap[i][index][2..3]
-        third_row << braille_wrap[i][index][4..5]
+      @i += 1
+      braille_wrap[@i].each_with_index do |data, index|
+        first_row << braille_wrap[@i][index][0..1]
+        second_row << braille_wrap[@i][index][2..3]
+        third_row << braille_wrap[@i][index][4..5]
       end
       result << first_row.join + "\n"
       result << second_row.join + "\n"
@@ -57,14 +62,6 @@ class NightWrite
     end
     result.join
   end
-
-
-  # def output_braille_in_three_rows(data)
-  #   puts data.join("\n")
-  # end
-
-
-# translator = EnglishToBraille.new
 
 
 end
--- lib/write.rb
+++ lib/write.rb
@@ -1,14 +1,15 @@
-require './lib/code.rb'
+require './lib/night_write.rb'
+require 'pry'
+
 handle = File.open(ARGV[0], "r")
-incoming_text = handle.read
-
+incoming_text = handle.read.gsub("\n", " ").chop
 handle.close
-etb = EnglishToBraille.new
-etb.english_returns_braille_array(incoming_text)
-#puts incoming_text
-require 'pry'; binding.pry
 
-=begin
-need to input incoming_text into my code file as the parameter
-'words'
-=end
+etb = NightWrite.new
+etb.english_returns_braille_array(incoming_text)
+etb.wrap_every_eighty_strings(etb.braille)
+etb.converts_braille_hash_to_braille(etb.braille_wrap)
+
+writer = File.open(ARGV[1], "w")
+writer.write(etb.result.join)
+writer.close
